29de8f3c87bbde7c10e2429ff6f74298bfa56947
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-09-23 16:32:43 +0600

    OOP initialized

---
+++ Basics/Parallelogram.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Parallelogram {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        for(int i = 1 ; i <= row; i++){
+            for (int k = col - i; k >= 1; k--) {  // This for loop is for Full Pyramid
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= col; j++){
+               if(i== 1 || j ==1 || i == row || j == col){
+                System.out.print(" 9");
+               }else{
+                System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ OOP/OopJava1.java
@@ -0,0 +1,6 @@
+public class OopJava1 {
+    int x = 5;
+    public static void main (String[] args){
+        System.out.println(new OopJava1().x);
+    }
+}
