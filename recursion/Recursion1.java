9b70e56bc7e92974250dbdc0ab25b0deb85b1244
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-10-11 20:06:23 +0600

    initialize recursion

---
+++ recursion/Recursion1.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class Recursion1 {
+    public static void main(String args[]) {
+        System.out.println("hello");
+        factorial(100);
+
+    }
+
+    static void factorial(int n) {
+        // Base case
+        if (n == 0) {
+            return;
+        }
+
+        System.err.println(n);
+        factorial(n - 1);
+    }
+
+}
