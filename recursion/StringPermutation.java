493d39e068d123146e62f0ad1d0ce59b5845bd30
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-10-20 22:34:35 +0600

    Recursion: String Permutation

---
+++ recursion/StringPermutation.java
@@ -0,0 +1,20 @@
+package recursion;
+
+
+public class StringPermutation {
+    public static void printPermutation(String str, String permutation){
+        if(str.length() ==0){
+            System.out.println(permutation);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i) ;
+            String newString = str.substring(0, i)+ str.substring(i+1);
+            printPermutation(newString, permutation+currentChar);
+        }
+        
+    }
+    public static void main(String args[]) {
+        printPermutation("abc", "");
+    }
+}
