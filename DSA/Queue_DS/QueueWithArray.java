cbcf9629fc618d5b4f08139ceb19c639d5a1f70b
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-01-22 23:22:32 +0600

    Queue DS with Array

---
+++ DSA/Queue_DS/QueueWithArray.java
@@ -0,0 +1,67 @@
+package DSA.Queue_DS;
+
+public class QueueWithArray {
+    static class Queue {
+        static int size;
+        static int[] arr;
+        static int rear = -1;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+        // Enqueue
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Full Queue");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        // Dequeue
+        public static int remove() {
+            if (isEmpty()) {
+
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // Dequeue
+        public static int peek() {
+            if (isEmpty()) {
+
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            int front = arr[0];
+            return front;
+        }
+    }
+
+    public static void main(String args[]) {
+        Queue q = new Queue(4);
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+    }
+}
