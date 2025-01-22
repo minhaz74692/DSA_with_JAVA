38a1eb91a7b0f91cf1ce43a9ff4c12d468bab7da
minhaz74692 <minhaz.ipe.sust@gmail.com>
2025-01-22 16:14:54 +0600

    Reverse a stack by recursion

---
+++ DSA/Stack/ReverseAStack.java
@@ -0,0 +1,40 @@
+package DSA.Stack;
+
+import java.util.*;
+
+public class ReverseAStack {
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+      
+        reverseStack(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+
+    }
+}
